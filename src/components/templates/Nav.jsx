ef069148b00f033d92c7ce6fb0d7ca47316aceec
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-02 12:47:38 -0300

    update: Nav style and items defined

--- src/components/templates/Nav.jsx
+++ src/components/templates/Nav.jsx
@@ -5,5 +5,12 @@ import React from 'react'
 
 export default props =>
     <aside className="menu-area">
-        Navegação
+        <nav className='menu'>
+            <a href="#">
+                <i className='fa fa-home'></i> Inicio
+            </a>
+            <a href="#/users">
+                <i className='fa fa-users'></i> Usuarios
+            </a>
+        </nav>
     </aside>
