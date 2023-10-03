c93e51de7e2578a2cbe46a555ca7b26a233f1dd2
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-03 08:54:05 -0300

    added: Main component and style

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -8,6 +8,8 @@ export default props =>
     <React.Fragment>
         <Header {...props} />
         <main className="content">
-            Conteúdo
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
         </main>
     </React.Fragment>
