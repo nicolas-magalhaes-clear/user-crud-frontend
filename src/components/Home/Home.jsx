eea374f6ee8ebb587ec0a3159e3cdbab54cbed61
Nicolas Magalhaes <nicolas.magalhaes.clear@gmail.com>
2023-10-03 08:53:36 -0300

    added: Home.jsx

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Main from "../templates/Main";
+
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Segundo projeto do capítulo de react">
+        <div className="display-4">
+            Bem vindo!
+        </div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+    </Main>
